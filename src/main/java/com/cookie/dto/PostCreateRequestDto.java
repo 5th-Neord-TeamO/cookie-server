7a30753e9570ae4a16218ca73d3204e36dbe65cb
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-11-26 02:02:40 +0900

    fix: 게시글 작성 API request에서 imgUrl을 포함하도록 수정

--- src/main/java/com/cookie/dto/PostCreateRequestDto.java
+++ src/main/java/com/cookie/dto/PostCreateRequestDto.java
@@ -5,5 +5,20 @@ import lombok.Getter;
 @Getter
 public class PostCreateRequestDto {
     private String title;
-    private String content;
+    private String imgUrl;
+    private String description;
 }
+
+// 글만 올리는 경우
+//{
+//    “title”: “dd”,
+//    “imgUrl”: null,
+//    “description”: “dd”
+//}
+
+// 이미지만 올리는 경우
+//{
+//    “title”: null,
+//    “imgUrl”: "dd",
+//    “description”: null
+//}
