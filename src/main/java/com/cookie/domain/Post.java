c0bfdb351f2b4f5cdeacf613d18064075b43112a
Minji Kim <minji1289@naver.com>
2023-11-25 19:38:48 +0900

    [feat] Post 컬럼 수정

--- src/main/java/com/cookie/domain/Post.java
+++ src/main/java/com/cookie/domain/Post.java
@@ -15,31 +15,25 @@ public class Post extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
-
-    @Column(length=200)
-    private String content;
-
-    @Setter
-    private String image;
-
-    @Setter
-    private String video;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
+    private String title;
+
+    @Column(length=200)
+    private String content;
+
     @Builder
-    public Post(Board board, String title, String content, Member member){
+    public Post(Board board, Member member, String title, String content){
         this.board = board;
+        this.member = member;
         this.title = title;
         this.content = content;
-        this.member = member;
     }
 
 }
