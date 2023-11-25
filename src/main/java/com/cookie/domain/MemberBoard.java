645981955f92f2fb56410ade2c78a7dc92a701bd
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-11-25 19:29:09 +0900

    feat: MemberBoard 엔티티 생성

---
+++ src/main/java/com/cookie/domain/MemberBoard.java
@@ -0,0 +1,32 @@
+package com.cookie.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+
+@Entity
+public class MemberBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="board_id")
+    private Board board;
+
+    @Builder
+    public MemberBoard(Member member, Board board) {
+        this.member = member;
+        this.board = board;
+    }
+}
